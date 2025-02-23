569453f717c1862046e8200c3a5575752ac0579b
Stephen Choate <68671251+schoate7@users.noreply.github.com>
2025-02-23 06:16:57 -0500

    Initial Commit

---
+++ BasicListTests/BasicListTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicListTests.swift
+//  BasicListTests
+//
+//  Created by Stephen Choate on 2/23/25.
+//
+
+import Testing
+@testable import BasicList
+
+struct BasicListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
